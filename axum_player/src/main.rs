e09a9580c84e99e433a27a5f5e1d51896a67c468
Aleksandr <108358595+KortesKnight@users.noreply.github.com>
2025-06-29 17:22:17 +0400

    Обновил сервис Rust

--- axum_player/src/main.rs
+++ axum_player/src/main.rs
@@ -1,12 +1,21 @@
-use axum::{Router, routing::get};
+use axum::{Router, http::StatusCode, response::IntoResponse, routing::get};
+use dotenvy::dotenv;
 
 #[tokio::main]
 async fn main() {
+    dotenv().map_err(|e| eprintln!("{e}")).ok();
+
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, Axum!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, Axum!" }))
+        .route("/health", get(health));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn health() -> impl IntoResponse {
+    StatusCode::OK
 }
