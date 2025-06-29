abfa252ffb652823187afa2ecfcd8d320d369731
Aleksandr <108358595+KortesKnight@users.noreply.github.com>
2025-06-29 20:30:03 +0400

    Исправлен баг с сигнатурой хендлера

--- axum_player/src/game.rs
+++ axum_player/src/game.rs
@@ -1,3 +1,11 @@
-pub async fn start_game() -> anyhow::Result<()> {
+use axum::Extension;
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
+use crate::AppState;
+
+pub async fn start_game(
+    Extension(app_state): Extension<Arc<RwLock<AppState>>>,
+) -> anyhow::Result<()> {
     Ok(())
 }
--- axum_player/src/handles.rs
+++ axum_player/src/handles.rs
@@ -1,6 +1,7 @@
 use axum::{Extension, response::IntoResponse};
 use reqwest::StatusCode;
 use std::sync::Arc;
+use tokio::sync::RwLock;
 use tracing::info;
 
 use crate::AppState;
@@ -14,10 +15,12 @@ pub async fn health() -> impl IntoResponse {
     (StatusCode::OK, "I'm good")
 }
 
-pub async fn start_game(Extension(app_state): Extension<Arc<AppState>>) -> impl IntoResponse {
+pub async fn start_game(
+    Extension(app_state): Extension<Arc<RwLock<AppState>>>,
+) -> impl IntoResponse {
     info!("Starting the game!");
 
-    match app_state.services_ready {
+    match app_state.read().await.services_ready() {
         true => (StatusCode::OK, "Starting the game!"),
         false => (
             StatusCode::METHOD_NOT_ALLOWED,
