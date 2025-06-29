5f2b8a29356cf512bff5e66f6c260a0e3b507bb1
Aleksandr <108358595+KortesKnight@users.noreply.github.com>
2025-06-29 21:12:43 +0400

    Добавлена заглушка для основного алгоритма

--- axum_player/src/game.rs
+++ axum_player/src/game.rs
@@ -1,11 +1,17 @@
-use axum::Extension;
 use std::sync::Arc;
 use tokio::sync::RwLock;
+use tracing::info;
 
-use crate::AppState;
+use crate::{AppState, ServiceState};
 
-pub async fn start_game(
-    Extension(app_state): Extension<Arc<RwLock<AppState>>>,
-) -> anyhow::Result<()> {
+pub async fn start_attack_game(app_state: Arc<RwLock<AppState>>) -> anyhow::Result<()> {
+    let targets = app_state.read().await.targets().clone();
+
+    info!("Targets: {:?}", targets);
+
+    let mut app = app_state.write().await;
+
+    app.set_service_state(ServiceState::Game);
+
     Ok(())
 }
