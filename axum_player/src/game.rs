b882b83f96dfd6709b16732352f3b63d6ff54173
Aleksandr <108358595+KortesKnight@users.noreply.github.com>
2025-07-20 14:23:36 +0400

    Переработка логики игры и атаки

--- axum_player/src/game.rs
+++ axum_player/src/game.rs
@@ -3,40 +3,54 @@ use std::sync::{
     Arc,
     atomic::{AtomicBool, Ordering},
 };
+use tokio::{task::JoinSet, time::Duration};
 use tracing::{debug, error, info};
 
 use crate::GameSettings;
 
 pub async fn start_attack_game(settings: GameSettings) -> anyhow::Result<()> {
-    let is_playing = Arc::new(AtomicBool::new(true));
-
-    let is_playing_ref = is_playing.clone();
-    tokio::task::spawn(async move {
-        tokio::time::sleep(tokio::time::Duration::from_secs(settings.round_sec as u64)).await;
-        is_playing_ref.store(false, Ordering::Relaxed);
-    });
-
     let settings = Arc::new(settings);
 
-    for target in settings.targets.clone() {
-        let client = {
-            let client = Client::builder()
-                .build()
-                .inspect_err(|e| error!("Error during building client: {e}"))?;
+    let is_playing = Arc::new(AtomicBool::new(true));
 
-            Arc::new(client)
-        };
+    let client = {
+        let client = Client::builder()
+            .build()
+            .inspect_err(|e| error!("Error during building client: {e}"))?;
+
+        Arc::new(client)
+    };
+
+    let mut targets = JoinSet::new();
+
+    for target in settings.targets.clone() {
         let is_playing = is_playing.clone();
+        let client = client.clone();
         let settings = settings.clone();
-        tokio::spawn(async move {
-            attack_target_new(
-                is_playing,
-                settings,
-                client,
-                format!("http://{target}/defence"),
-            )
-            .await
+        let target = Arc::new(format!("http://{target}/defence"));
+
+        // Main attack task
+        targets.spawn(async move {
+            // Stopping algorithm
+            {
+                let is_playing = is_playing.clone();
+                let settings = settings.clone();
+                let target = target.clone();
+                tokio::spawn(async move {
+                    tokio::time::sleep(Duration::from_secs(settings.round_sec as u64)).await;
+                    is_playing.store(false, Ordering::Relaxed);
+                    info!("Time is up for target {}", target);
+                });
+            }
+
+            attack_target_new(is_playing, settings, client, target).await;
         });
+    }
+
+    while let Some(res) = targets.join_next().await {
+        if let Err(err) = res {
+            error!("JoinSet error: {:?}", err);
+        }
     }
 
     Ok(())
@@ -46,22 +60,20 @@ async fn attack_target_new(
     is_playing: Arc<AtomicBool>,
     settings: Arc<GameSettings>,
     client: Arc<Client>,
-    target: String,
-) -> anyhow::Result<()> {
-    let target = Arc::new(target);
+    target: Arc<String>,
+) {
+    let mut attackers = JoinSet::new();
 
     for idx in 0..settings.connections_amount {
+        let is_playing = is_playing.clone();
         let client = client.clone();
         let target = target.clone();
-        let is_playing = is_playing.clone();
         let settings = settings.clone();
 
-        debug!("Attacker with target {target} idx {idx}");
+        attackers.spawn(async move {
+            debug!("Attacker with target {target} idx {idx} is playing!");
 
-        tokio::spawn(async move {
             while is_playing.load(Ordering::Relaxed) {
-                debug!("Attacker with target {target} idx {idx} is playing!");
-
                 match client
                     .get(&*target)
                     .body(settings.difficulties.to_string())
@@ -97,7 +109,13 @@ async fn attack_target_new(
         });
     }
 
-    Ok(())
+    while let Some(res) = attackers.join_next().await {
+        if let Err(err) = res {
+            error!("JoinSet error: {:?}", err);
+        }
+    }
+
+    info!("Attack on target {target} is finished!");
 }
 
 pub fn fibonacci_iterative(n: usize) -> usize {
