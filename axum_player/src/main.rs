3ef66641e83e8625e8516605dfb92e7a972b83e9
Aleksandr <108358595+KortesKnight@users.noreply.github.com>
2025-07-20 14:30:24 +0400

    Подключил хендлер

--- axum_player/src/main.rs
+++ axum_player/src/main.rs
@@ -7,7 +7,7 @@ use std::str::FromStr;
 use tracing::{Level, info, warn};
 use tracing_subscriber::FmtSubscriber;
 
-use axum_player::handles::{health, ready, start};
+use axum_player::handles::{defense, health, ready, start};
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> anyhow::Result<()> {
@@ -30,7 +30,8 @@ async fn main() -> anyhow::Result<()> {
         .route("/", get(|| async { "Hello, Axum!" }))
         .route("/health", get(health))
         .route("/ready", get(ready))
-        .route("/start", post(start));
+        .route("/start", post(start))
+        .route("/defense/{difficulties}", post(defense));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
