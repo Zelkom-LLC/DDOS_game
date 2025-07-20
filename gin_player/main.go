8f039dedf98e3eb0fbac334ab04a8fd29ba25649
Aleksandr <108358595+KortesKnight@users.noreply.github.com>
2025-07-20 18:13:06 +0400

    Переработка main

--- gin_player/main.go
+++ gin_player/main.go
@@ -12,37 +12,47 @@ import (
 )
 
 func main() {
-    // загружаем .env (если не найден — просто логируем и идём дальше)
-    if err := godotenv.Load(); err != nil {
-        log.Println("No .env file found:", err)
-    }
+	// загружаем .env (если не найден — просто логируем и идём дальше)
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found:", err)
+	}
 
-    // создаём роутер с дефолтными middleware (логирование, recovery)
-    r := gin.Default()
+	// создаём роутер с дефолтными middleware (логирование, recovery)
+	r := gin.Default()
 
-    // корневой маршрут
-    r.GET("/", func(c *gin.Context) {
-        c.String(http.StatusOK, "Hello, Gin!")
-    })
+	// корневой маршрут
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, Gin!")
+	})
 
-    // health-check маршрут
-    r.GET("/health", health)
+	// health-check маршрут
+	r.GET("/health", health)
 
-    r.GET("/ready", func(c *gin.Context) {
-        c.String(http.StatusOK, "Ready!")
-    })
+	r.GET("/ready", func(c *gin.Context) {
+		c.String(http.StatusOK, "Ready!")
+	})
 
-    r.GET("/ping", func(c *gin.Context) {
-        c.String(http.StatusOK, "Pong!")
-    })
+	r.POST("/defense/:attack", defense)
 
-    // запускаем сервер на 0.0.0.0:3000
-    if err := r.Run(":8000"); err != nil {
-        log.Fatalf("Ошибка запуска сервера: %v", err)
-    }
+	// запускаем сервер на 0.0.0.0:3000
+	if err := r.Run(":8000"); err != nil {
+		log.Fatalf("Ошибка запуска сервера: %v", err)
+	}
 }
 
 // handler для /health
 func health(c *gin.Context) {
-    c.Status(http.StatusOK)
+	c.Status(http.StatusOK)
+}
+
+func defense(c *gin.Context) {
+	attackStr := c.Param("attack")
+	attack, err := strconv.ParseUint(attackStr, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid number"})
+		return
+	}
+
+	result := game.Fibonacci_iterative(attack)
+	c.String(http.StatusOK, "Defense! - %d", result)
 }
